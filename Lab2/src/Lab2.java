6ec1541c5c54f978936b1358fffa4bb22f7a99cf
Morgan <morganw2001@gmail.com>
2021-01-22 16:49:54 -0500

    begin lab 2

---
+++ Lab2/src/Lab2.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class Lab2 {
+
+    /**
+     * Write a function called isBalanced that takes a string of braces as input and returns true if
+     * the braces in the string are balanced. If there are more ‘{’ than ‘}’ or more ‘}’ than ‘{’ in the
+     * string, the function should return false. You should make use of the built-in java
+     * implementation of the stack data structure under java.util.Stack .
+     */
+
+    public static void main (String [] args){
+
+
+    }
+
+
+    public static boolean isBalanced(String str){ // look up java.util.Stack on java docs
+        Stack<Character> stack = new Stack <Character>();
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
