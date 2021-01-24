09e55c86ed6eeed77fa0b53b5ec6070104d267c9
Morgan <morganw2001@gmail.com>
2021-01-23 19:37:32 -0500

    done with lab 2

--- Lab2/src/Lab2.java
+++ Lab2/src/Lab2.java
@@ -2,52 +2,40 @@ import java.util.Stack;
 
 public class Lab2 {
 
-    /**
-     * Write a function called isBalanced that takes a string of braces as input and returns true if
-     * the braces in the string are balanced. If there are more ‘{’ than ‘}’ or more ‘}’ than ‘{’ in the
-     * string, the function should return false. You should make use of the built-in java
-     * implementation of the stack data structure under java.util.Stack .
-     */
-
     public static void main (String [] args){
-        String str = "{}{{{{}}"; // this test works
-        String str2 = "{{{}}}";
-        boolean ans = isBalanced(str2);
-        System.out.println("Answer " + ans); // should print false
-
+        // first example from lab instructions.
+        String str = "{}{{{{}}";
+        //String str2 = "{{{}}}"; // second example from lab instructions.
+        boolean ans = isBalanced(str); //replace with str2 when testing second example.
+        System.out.println("Answer " + ans); //should print false for str and true for str2.
     }
 
-
-    public static boolean isBalanced(String str){ // look up java.util.Stack on java docs
+    public static boolean isBalanced(String str){
+        // initializes stack object
         Stack<Character> stack = new Stack <Character>();
-        int l=0; // variable to count left braces
-        int r=0; //variable to count right braces
+        // variable to count left braces
+        int l=0;
+        //variable to count right braces
+        int r=0;
+        //for loop to iterate through the string check the braces
         for (int i =0; i <str.length(); i++){
-            stack.push(str.charAt(i));
-            if (str.charAt(i) == '{'){
+            stack.push(str.charAt(i)); // pushes string values to the stack
+            if (str.charAt(i) == '{'){ //checks for a left brace
                 stack.pop();
-                l++;
+                l++; //increases if there is a left brace
             }
-            else if (str.charAt(i) == '}') {
+            else if (str.charAt(i) == '}') { //checks for a right brace
                 stack.pop();
-                r++;
+                r++; //increases if there is a right brace
             }
         }
-
-        if (l==r){
-
+        if (l==r){ // returns true if there are an equal amount of left and right braces.
             return true;
         }
-        else{
+        else { // returns false if there is not an equal amount of left and right braces.
             return false;
         }
 
     }
-
-
-
-
-
-
 }
 
