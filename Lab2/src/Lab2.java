7d4a04aa05094baf6136dc42389c37e630b34d1d
Morgan <morganw2001@gmail.com>
2021-01-23 19:13:23 -0500

    done with lab 2

--- Lab2/src/Lab2.java
+++ Lab2/src/Lab2.java
@@ -10,16 +10,37 @@ public class Lab2 {
      */
 
     public static void main (String [] args){
-
+        String str = "{}{{{{}}"; // this test works
+        String str2 = "{{{}}}";
+        boolean ans = isBalanced(str2);
+        System.out.println("Answer " + ans); // should print false
 
     }
 
 
     public static boolean isBalanced(String str){ // look up java.util.Stack on java docs
         Stack<Character> stack = new Stack <Character>();
+        int l=0; // variable to count left braces
+        int r=0; //variable to count right braces
+        for (int i =0; i <str.length(); i++){
+            stack.push(str.charAt(i));
+            if (str.charAt(i) == '{'){
+                stack.pop();
+                l++;
+            }
+            else if (str.charAt(i) == '}') {
+                stack.pop();
+                r++;
+            }
+        }
 
+        if (l==r){
 
-
+            return true;
+        }
+        else{
+            return false;
+        }
 
     }
 
