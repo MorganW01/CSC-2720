018f5ea6f30fd34746c7d1bd17d5798a669e8afc
Morgan <morganw2001@gmail.com>
2021-01-30 11:01:24 -0500

    started lab 3

---
+++ Lab3/src/Lab3.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+public class Lab3 {
+
+    public static void main (String[] args){ String
+            s1 = "DataStructuresIssss###Fun"; String
+            s2 = "DataStructuresIszwp###Fun"; boolean
+            ans = backspaceCompare(s1,s2);
+        System.out.println(ans); // Should be True
+    }
+
+
+
+}
