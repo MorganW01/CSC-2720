6bf7f89b1efce6981de960c4f86efb1f8d7b6931
Morgan <morganw2001@gmail.com>
2021-02-06 15:33:46 -0500

    done w lab 4

--- Lab4/src/Lab4.java
+++ Lab4/src/Lab4.java
@@ -3,29 +3,50 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /* A palindrome is a string of characters that reads the same from left to right as it does from right to left.
-* Write  a function called checkPalindrome that takes a string s1 and evaluate if the string is a palindrome
-* or not  using only a stack and a queue data structures. You should make use of the built-in java implementation of
-* the stack and queue data structure under java.util.Stack and java.util.Queue . */
+ * Write  a function called checkPalindrome that takes a string s1 and evaluate if the string is a palindrome
+ * or not  using only a stack and a queue data structures. You should make use of the built-in java implementation of
+ * the stack and queue data structure under java.util.Stack and java.util.Queue . */
 
 
 public class Lab4 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+
         String s1 = "radar";
-        boolean ans = checkPalindrome(s1);
-        System.out.println(ans); // Should be True
+        boolean answer1 = checkPalindrome(s1);
+        System.out.println("Answer 1: " + answer1); // Should be True
+
+        String s2 = "HitTheRoadJack";
+        boolean answer2 = checkPalindrome(s2);
+        System.out.println("Answer 2: " + answer2); //Should be False
     }
 
-    public static boolean checkPalindrome(String s1){
+    public static boolean checkPalindrome(String string) {
         Stack<Character> s = new Stack<Character>();
         Queue<Character> q = new LinkedList<Character>();
-            // Example of stack push s.push("r")
-            // Example of stack pop s.pop()
-            // Example of queue enqeue q.add("r")
-            // Example of queue dequeue q.poll()
-            // Check if stack or queue is empty s.isEmpty() , q.isEmpty()
-            // INSERT YOUR CODE HERE
 
-        return false; //placeholder
+        //for loop goes through all the characters in the string and adds a char to both the stack and the queue.
+        for (int i = 0; i < string.length(); i++) {
+            s.push(string.charAt(i)); //puts the characters into the stack from first to last.
+            q.add(string.charAt(i)); // adds the data in the queue from beginning to end
+        }
+
+        boolean answer = true;
+
+        // goes through the loop while the queue is empty (until the queue is empty)
+        while (q.isEmpty() == false) {
+            // queue will poll elements from the beginning of the string and stack will pop elements from the end of the string
+            // checks if they are the same and if they are it will continue
+            if (q.poll() == s.pop())
+                continue;
+
+            // and if not it returns false.
+            answer = false;
+            break;
+        }
+
+        return answer;
+
+
     }
 }
 
