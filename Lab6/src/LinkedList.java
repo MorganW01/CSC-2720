7bb7447e2275f7a34ca37ccabc9a04ed43470840
Morgan <morganw2001@gmail.com>
2021-02-20 10:57:38 -0500

    done

--- Lab6/src/LinkedList.java
+++ Lab6/src/LinkedList.java
@@ -2,22 +2,41 @@
 
 public class LinkedList {
     static Node head;
+
     static class Node {
         int data;
         Node next;
-        Node(int d) //Node Constructor
-        {
+
+        Node(int d) {//Node Constructor
+
             data = d;
             next = null;
         }
     }
     /* Function to reverse the linked list */
     Node reverse(Node node) {
-        //Write Your Code Here
+
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null){
+            next = current.next;
+            current.next=prev;
+            prev = current;
+            current=next;
+        }
+
+        node = prev;
+        return node;
+
     }
     // prints content of double linked list
     void printList(Node node) {
-        //Write Your Code Here
+        while(node != null){
+            System.out.println(node.data + " ");
+            node = node.next;
+        }
+
     }
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
@@ -25,11 +44,13 @@ public class LinkedList {
         list.head.next = new Node(2);
         list.head.next.next = new Node(3);
         list.head.next.next.next = new Node(4);
+
         System.out.println("Given Linked list");
         list.printList(head);
         head = list.reverse(head);
         System.out.println("");
         System.out.println("Reversed linked list ");
         //Write Your Code to print the linked list Here.
+        list.printList(head);
     }
 }
