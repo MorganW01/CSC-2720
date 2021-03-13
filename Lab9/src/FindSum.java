e30414dbfdf6b91ad5d7281837b7049a4846f21e
Morgan <morganw2001@gmail.com>
2021-03-13 15:51:17 -0500

    start

---
+++ Lab9/src/FindSum.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+public class FindSum {
+
+    public static void main(String [] args){
+        int[] array = { 1, 5, 4, 6, 7, 9 };
+        HashSet<Integer> res = findSums(array);
+        System.out.println(res.toString()); // Should return [6,5,7,9]
+    }
+    public static HashSet<Integer> findSums(int[] elements){
+        HashSet<Integer> sums = new HashSet<Integer>();
+
+
+        return sums;
+    }
+}
