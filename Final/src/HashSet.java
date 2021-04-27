ae91bd590b76bfa1587c894f1e6b9b7a24c43b01
Morgan <morganw2001@gmail.com>
2021-04-27 16:01:17 -0400

    done

--- Final/src/HashSet.java
+++ Final/src/HashSet.java
@@ -5,5 +5,65 @@
  * Final Exam Question 2
  * */
 
+public class HashSet {
+    //creates hashset array of type boolean.
+    boolean[] hashsetArray;
 
+    HashSet(){
+        //sets the size of the boolean to 500 (random number used so the array is big enough for elements to be added)
+        hashsetArray = new boolean [500];
+    }
+
+    public static void main (String [] args){
+        //HashSet object
+        HashSet hashSet = new HashSet();
+
+        //adds 4 elements to the HashSet
+        hashSet.addElement(5);
+        hashSet.addElement(10);
+        hashSet.addElement(15);
+        hashSet.addElement(20);
+
+        //creates contains integer variable to check whether or not an element is in the hashSet.
+        int contains = hashSet.containsElement(35);
+        if (contains == 0){ // if returned int from containsElement is -1, then it's not in the hashSet
+            System.out.println("Element not in hashset");
+        }
+        else { // if not, it's in the hashSet
+            System.out.println("The element " +contains + " is in this hashset");
+        }
+        //creates contains integer variable to check whether or not an element is in the hashSet and can be removed.
+        int remove = hashSet.removeElement(15);
+        if (remove == 0){ // if returned int = 0, element does not exist
+            System.out.println("Element does not exist");
+        }
+        else { // if not, it is in the hashset and it's removed
+            System.out.println("Element removed");
+        }
+    }
+
+    public int containsElement(int element){
+        //checks if element is in array, returns the element if it is.
+        if (hashsetArray[element]){
+            return element;
+        }
+        else { // returns 0 if element is not in the hashset.
+            return 0;
+        }
+    }
+
+    public void addElement (int element) { //adds element to the hashset by making the element true.
+        hashsetArray[element] = true;
+    }
+
+    public int removeElement (int element){
+        if (hashsetArray[element]){ //checks if element is true/ present in the array
+            hashsetArray[element]=false; //removes it by making the element false.
+            return 1;
+        }
+        else { // if not in array, it returns 0
+            return 0;
+        }
+    }
+}
 
--- Final/src/MinMaxHeap.java
+++ Final/src/MinMaxHeap.java
@@ -6,43 +6,42 @@
 * */
 public class MinMaxHeap {
 
-    public void sort(int arr[]) {
-        int n = arr.length;
+    public void sort(int array[]) {
+        int n = array.length;
         // Build heap (rearrange array)
         for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
+            heapify(array, n, i);
 
     }
 
-    void heapify(int arr[], int n, int i) {
+    void heapify(int array[], int n, int i) {
         int largest = i; // Initialize largest as root
         int l = 2*i + 1; // left = 2*i + 1
         int r = 2*i + 2; // right = 2*i + 2
 
         // If left child is larger than root
-        if (l < n && arr[l] > arr[largest])
+        if (l < n && array[l] > array[largest])
             largest = l;
 
         // If right child is larger than largest so far
-        if (r < n && arr[r] > arr[largest])
+        if (r < n && array[r] > array[largest])
             largest = r;
 
         // If largest is not root
         if (largest != i)
         {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
 
             // Recursively heapify the affected sub-tree
-            heapify(arr, n, largest);
+            heapify(array, n, largest);
         }
     }
 
     public static void main(String [] args){
 
         int[] array = {3, 5, 9, 6, 8, 20, 10, 12, 18, 9};
-        int arrayLength = array.length;
 
         MinMaxHeap x = new MinMaxHeap();
 
